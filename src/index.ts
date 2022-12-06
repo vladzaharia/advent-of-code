3d225a5e3af66a4df212b48b9979c9a583c6cc75
Vlad Zaharia <hey@vlad.gg>
2022-12-05 23:25:50 -0800

    Better execution

--- src/index.ts
+++ src/index.ts
@@ -10,40 +10,47 @@ interface RunnerArgs {
     verbose?: boolean;
 }
 
+// Create run command
 let argv = yargs
-    .command('run', "Run scripts.", (yargs: Argv) => {
+    .command('$0 [path]', "Run Advent of Code 2022 scripts. By default, will run all scripts in src.", (yargs: Argv) => {
         return yargs.option('path', {
             describe: "Full path to run",
             type: 'string',
-            alias: 'p'
+            alias: 'p',
+            conflicts: ['day', 'part']
         }).option('day', {
             describe: "The day of AoC to run",
             type: 'number',
-            alias: 'd'
+            alias: 'd',
+            conflicts: ['path']
         }).option('part', {
             describe: "The part to run",
             type: 'number',
-            alias: 'n'
+            alias: 'n',
+            conflicts: ['path']
         }).option('verbose', {
             describe: "Verbose logging",
             type: 'boolean',
-            alias: 'v'
+            alias: ['v', 'V']
         }).help()
-    }).demandCommand(1).argv as { [key in keyof Arguments<RunnerArgs> as key | CamelCaseKey<key>]: Arguments<RunnerArgs>[key] };
+    }).argv as { [key in keyof Arguments<RunnerArgs> as key | CamelCaseKey<key>]: Arguments<RunnerArgs>[key] };
 
 if (argv.verbose) {
-    // TODO: pass through a verbose flag into all the main functions
-    console.info("Verbose mode on.");
+    console.info("\x1b[1m\x1b[36m[!] Verbose mode on.\x1b[0m");
+    console.log(JSON.stringify(argv));
 }
 
 if (argv.path) {
+    // Execute the provided path, as long as it's a recognized AoC path
     executeAdventFile(populateAdventFile({ ... getAdventFileFromPath(argv.path, argv.verbose)!, base: __dirname }, argv.verbose), argv.verbose);
 } else if (argv.day && argv.part) {
+    // Create advent file based off day and part
     executeAdventFile(populateAdventFile({ 
         base: __dirname,
         day: argv.day,
         part: argv.part
     }, argv.verbose), argv.verbose);
 } else {
+    // No arguments, run all scripts
     getAllScripts(__dirname, argv.verbose).forEach(async (f) => await executeAdventFile(f, argv.verbose));
 }
